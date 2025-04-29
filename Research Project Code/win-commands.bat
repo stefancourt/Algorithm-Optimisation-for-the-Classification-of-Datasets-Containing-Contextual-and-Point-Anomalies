aa0916e7f3e8a607bc28fc3da93e2604cea3b092
Stefan Court <stefanandrewcourt@gmail.com>
2025-04-29 19:39:06 +0100

    Update win-commands.bat

--- Research Project Code/win-commands.bat
+++ Research Project Code/win-commands.bat
@@ -1,19 +1,19 @@
 @echo off
 
 REM Create virtual environment
-python3 -m venv venv
+python -m venv venv
 
 REM Activate virtual environment
 call venv\Scripts\activate
 
 REM Install ipykernel
-pip3 install ipykernel
+pip install ipykernel
 
 REM Allow use of virtual environment within notebook
-python3 -m ipykernel install --user --name=venv
+python -m ipykernel install --user --name=venv
 
 REM Install dependencies within the virtual environment
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 REM Install C dependencies to allow python file to run
 REM call C_files/ocsvm_exec.bat
